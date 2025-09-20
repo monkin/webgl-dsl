8f5f2c0d8928fa3ceaca05b0800347b78a87dc76
Andrei Monkin <monkin.andrey@gmail.com>
2025-09-21 01:17:29 +0300

    Extract WebGL constants into a separate file

--- src/webgl.ts
+++ src/webgl.ts
@@ -1,426 +1,110 @@
 import { command } from "./command";
 import { ProgramSource, SourceConfig, TypeMap } from "./dsl";
 import { Disposable, use, uses } from "./disposable";
+import {
+    // clear bits
+    DEPTH_BUFFER_BIT,
+    COLOR_BUFFER_BIT,
+    // primitive types
+    POINTS,
+    LINES,
+    LINE_LOOP,
+    LINE_STRIP,
+    TRIANGLES,
+    TRIANGLE_STRIP,
+    TRIANGLE_FAN,
+    // blending
+    ZERO,
+    ONE,
+    SRC_COLOR,
+    ONE_MINUS_SRC_COLOR,
+    SRC_ALPHA,
+    ONE_MINUS_SRC_ALPHA,
+    DST_ALPHA,
+    ONE_MINUS_DST_ALPHA,
+    DST_COLOR,
+    ONE_MINUS_DST_COLOR,
+    SRC_ALPHA_SATURATE,
+    FUNC_ADD,
+    FUNC_SUBTRACT,
+    FUNC_REVERSE_SUBTRACT,
+    // buffers
+    ARRAY_BUFFER,
+    ELEMENT_ARRAY_BUFFER,
+    STREAM_DRAW,
+    STATIC_DRAW,
+    DYNAMIC_DRAW,
+    // culling and toggles
+    FRONT,
+    BACK,
+    FRONT_AND_BACK,
+    CULL_FACE,
+    BLEND,
+    DEPTH_TEST,
+    SCISSOR_TEST,
+    // errors
+    NO_ERROR,
+    INVALID_ENUM,
+    INVALID_VALUE,
+    INVALID_OPERATION,
+    OUT_OF_MEMORY,
+    // point size
+    ALIASED_POINT_SIZE_RANGE,
+    // depth function values
+    NEVER,
+    LESS,
+    EQUAL,
+    LEQUAL,
+    GREATER,
+    NOTEQUAL,
+    GEQUAL,
+    ALWAYS,
+    // textures and pixel formats
+    TEXTURE_MAG_FILTER,
+    TEXTURE_MIN_FILTER,
+    TEXTURE_WRAP_S,
+    TEXTURE_WRAP_T,
+    TEXTURE_2D,
+    TEXTURE0,
+    CLAMP_TO_EDGE,
+    NEAREST,
+    LINEAR,
+    UNSIGNED_BYTE,
+    ALPHA,
+    RGB,
+    RGBA,
+    LUMINANCE,
+    LUMINANCE_ALPHA,
+    // shader types and uniforms
+    BOOL,
+    FLOAT,
+    FLOAT_VEC2,
+    FLOAT_VEC3,
+    FLOAT_VEC4,
+    FLOAT_MAT2,
+    FLOAT_MAT3,
+    FLOAT_MAT4,
+    SAMPLER_2D,
+    // shaders and programs
+    FRAGMENT_SHADER,
+    VERTEX_SHADER,
+    COMPILE_STATUS,
+    LINK_STATUS,
+    // attribute queries
+    ACTIVE_ATTRIBUTES,
+    // framebuffer/renderbuffer
+    FRAMEBUFFER,
+    RENDERBUFFER,
+    DEPTH_COMPONENT16,
+    COLOR_ATTACHMENT0,
+    DEPTH_ATTACHMENT,
+    UNPACK_FLIP_Y_WEBGL,
+    SRGB_EXT,
+    SRGB8_ALPHA8_EXT,
+    UNSIGNED_SHORT,
+    ACTIVE_UNIFORMS,
+} from "./webgl-consts";
 import WithoutPrecision = TypeMap.WithoutPrecision;
-
-/*
- * Not every server WebGL implementation has its own WebGLRenderingContext class.
- * Because of this, we have to declare constants manually.
- */
-
-/* ClearBufferMask */
-const DEPTH_BUFFER_BIT = 0x00000100;
-const STENCIL_BUFFER_BIT = 0x00000400;
-const COLOR_BUFFER_BIT = 0x00004000;
-
-/* BeginMode */
-const POINTS = 0x0000;
-const LINES = 0x0001;
-const LINE_LOOP = 0x0002;
-const LINE_STRIP = 0x0003;
-const TRIANGLES = 0x0004;
-const TRIANGLE_STRIP = 0x0005;
-const TRIANGLE_FAN = 0x0006;
-
-/* BlendingFactorDest */
-const ZERO = 0;
-const ONE = 1;
-const SRC_COLOR = 0x0300;
-const ONE_MINUS_SRC_COLOR = 0x0301;
-const SRC_ALPHA = 0x0302;
-const ONE_MINUS_SRC_ALPHA = 0x0303;
-const DST_ALPHA = 0x0304;
-const ONE_MINUS_DST_ALPHA = 0x0305;
-
-/* BlendingFactorSrc */
-/* ZERO */
-/* ONE */
-const DST_COLOR = 0x0306;
-const ONE_MINUS_DST_COLOR = 0x0307;
-const SRC_ALPHA_SATURATE = 0x0308;
-/* SRC_ALPHA */
-/* ONE_MINUS_SRC_ALPHA */
-/* DST_ALPHA */
-/* ONE_MINUS_DST_ALPHA */
-
-/* BlendEquationSeparate */
-const FUNC_ADD = 0x8006;
-const BLEND_EQUATION = 0x8009;
-const BLEND_EQUATION_RGB = 0x8009; /* same as BLEND_EQUATION */
-const BLEND_EQUATION_ALPHA = 0x883d;
-
-/* BlendSubtract */
-const FUNC_SUBTRACT = 0x800a;
-const FUNC_REVERSE_SUBTRACT = 0x800b;
-
-/* Separate Blend Functions */
-const BLEND_DST_RGB = 0x80c8;
-const BLEND_SRC_RGB = 0x80c9;
-const BLEND_DST_ALPHA = 0x80ca;
-const BLEND_SRC_ALPHA = 0x80cb;
-const CONSTANT_COLOR = 0x8001;
-const ONE_MINUS_CONSTANT_COLOR = 0x8002;
-const CONSTANT_ALPHA = 0x8003;
-const ONE_MINUS_CONSTANT_ALPHA = 0x8004;
-const BLEND_COLOR = 0x8005;
-
-/* Buffer Objects */
-const ARRAY_BUFFER = 0x8892;
-const ELEMENT_ARRAY_BUFFER = 0x8893;
-const ARRAY_BUFFER_BINDING = 0x8894;
-const ELEMENT_ARRAY_BUFFER_BINDING = 0x8895;
-
-const STREAM_DRAW = 0x88e0;
-const STATIC_DRAW = 0x88e4;
-const DYNAMIC_DRAW = 0x88e8;
-
-const BUFFER_SIZE = 0x8764;
-const BUFFER_USAGE = 0x8765;
-
-const CURRENT_VERTEX_ATTRIB = 0x8626;
-
-/* CullFaceMode */
-const FRONT = 0x0404;
-const BACK = 0x0405;
-const FRONT_AND_BACK = 0x0408;
-
-/* DepthFunction */
-/* NEVER */
-/* LESS */
-/* EQUAL */
-/* LEQUAL */
-/* GREATER */
-/* NOTEQUAL */
-/* GEQUAL */
-/* ALWAYS */
-
-/* EnableCap */
-/* TEXTURE_2D */
-const CULL_FACE = 0x0b44;
-const BLEND = 0x0be2;
-const DITHER = 0x0bd0;
-const STENCIL_TEST = 0x0b90;
-const DEPTH_TEST = 0x0b71;
-const SCISSOR_TEST = 0x0c11;
-const POLYGON_OFFSET_FILL = 0x8037;
-const SAMPLE_ALPHA_TO_COVERAGE = 0x809e;
-const SAMPLE_COVERAGE = 0x80a0;
-
-/* ErrorCode */
-const NO_ERROR = 0;
-const INVALID_ENUM = 0x0500;
-const INVALID_VALUE = 0x0501;
-const INVALID_OPERATION = 0x0502;
-const OUT_OF_MEMORY = 0x0505;
-
-/* FrontFaceDirection */
-const CW = 0x0900;
-const CCW = 0x0901;
-
-/* GetPName */
-const LINE_WIDTH = 0x0b21;
-const ALIASED_POINT_SIZE_RANGE = 0x846d;
-const ALIASED_LINE_WIDTH_RANGE = 0x846e;
-const CULL_FACE_MODE = 0x0b45;
-const FRONT_FACE = 0x0b46;
-const DEPTH_RANGE = 0x0b70;
-const DEPTH_WRITEMASK = 0x0b72;
-const DEPTH_CLEAR_VALUE = 0x0b73;
-const DEPTH_FUNC = 0x0b74;
-const STENCIL_CLEAR_VALUE = 0x0b91;
-const STENCIL_FUNC = 0x0b92;
-const STENCIL_FAIL = 0x0b94;
-const STENCIL_PASS_DEPTH_FAIL = 0x0b95;
-const STENCIL_PASS_DEPTH_PASS = 0x0b96;
-const STENCIL_REF = 0x0b97;
-const STENCIL_VALUE_MASK = 0x0b93;
-const STENCIL_WRITEMASK = 0x0b98;
-const STENCIL_BACK_FUNC = 0x8800;
-const STENCIL_BACK_FAIL = 0x8801;
-const STENCIL_BACK_PASS_DEPTH_FAIL = 0x8802;
-const STENCIL_BACK_PASS_DEPTH_PASS = 0x8803;
-const STENCIL_BACK_REF = 0x8ca3;
-const STENCIL_BACK_VALUE_MASK = 0x8ca4;
-const STENCIL_BACK_WRITEMASK = 0x8ca5;
-const VIEWPORT = 0x0ba2;
-const SCISSOR_BOX = 0x0c10;
-
-/* SCISSOR_TEST */
-const COLOR_CLEAR_VALUE = 0x0c22;
-const COLOR_WRITEMASK = 0x0c23;
-const UNPACK_ALIGNMENT = 0x0cf5;
-const PACK_ALIGNMENT = 0x0d05;
-const MAX_TEXTURE_SIZE = 0x0d33;
-const MAX_VIEWPORT_DIMS = 0x0d3a;
-const SUBPIXEL_BITS = 0x0d50;
-const RED_BITS = 0x0d52;
-const GREEN_BITS = 0x0d53;
-const BLUE_BITS = 0x0d54;
-const ALPHA_BITS = 0x0d55;
-const DEPTH_BITS = 0x0d56;
-const STENCIL_BITS = 0x0d57;
-const POLYGON_OFFSET_UNITS = 0x2a00;
-/* POLYGON_OFFSET_FILL */
-const POLYGON_OFFSET_FACTOR = 0x8038;
-const TEXTURE_BINDING_2D = 0x8069;
-const SAMPLE_BUFFERS = 0x80a8;
-const SAMPLES = 0x80a9;
-const SAMPLE_COVERAGE_VALUE = 0x80aa;
-const SAMPLE_COVERAGE_INVERT = 0x80ab;
-
-/* GetTextureParameter */
-/* TEXTURE_MAG_FILTER */
-/* TEXTURE_MIN_FILTER */
-/* TEXTURE_WRAP_S */
-/* TEXTURE_WRAP_T */
-
-const COMPRESSED_TEXTURE_FORMATS = 0x86a3;
-
-/* HintMode */
-const DONT_CARE = 0x1100;
-const FASTEST = 0x1101;
-const NICEST = 0x1102;
-
-/* HintTarget */
-const GENERATE_MIPMAP_HINT = 0x8192;
-
-/* DataType */
-const BYTE = 0x1400;
-const UNSIGNED_BYTE = 0x1401;
-const SHORT = 0x1402;
-const UNSIGNED_SHORT = 0x1403;
-const INT = 0x1404;
-const UNSIGNED_INT = 0x1405;
-const FLOAT = 0x1406;
-
-/* PixelFormat */
-const DEPTH_COMPONENT = 0x1902;
-const ALPHA = 0x1906;
-const RGB = 0x1907;
-const RGBA = 0x1908;
-const LUMINANCE = 0x1909;
-const LUMINANCE_ALPHA = 0x190a;
-
-/* PixelType */
-/* UNSIGNED_BYTE */
-const UNSIGNED_SHORT_4_4_4_4 = 0x8033;
-const UNSIGNED_SHORT_5_5_5_1 = 0x8034;
-const UNSIGNED_SHORT_5_6_5 = 0x8363;
-
-/* Shaders */
-const FRAGMENT_SHADER = 0x8b30;
-const VERTEX_SHADER = 0x8b31;
-const MAX_VERTEX_ATTRIBS = 0x8869;
-const MAX_VERTEX_UNIFORM_VECTORS = 0x8dfb;
-const MAX_VARYING_VECTORS = 0x8dfc;
-const MAX_COMBINED_TEXTURE_IMAGE_UNITS = 0x8b4d;
-const MAX_VERTEX_TEXTURE_IMAGE_UNITS = 0x8b4c;
-const MAX_TEXTURE_IMAGE_UNITS = 0x8872;
-const MAX_FRAGMENT_UNIFORM_VECTORS = 0x8dfd;
-const SHADER_TYPE = 0x8b4f;
-const DELETE_STATUS = 0x8b80;
-const LINK_STATUS = 0x8b82;
-const VALIDATE_STATUS = 0x8b83;
-const ATTACHED_SHADERS = 0x8b85;
-const ACTIVE_UNIFORMS = 0x8b86;
-const ACTIVE_ATTRIBUTES = 0x8b89;
-const SHADING_LANGUAGE_VERSION = 0x8b8c;
-const CURRENT_PROGRAM = 0x8b8d;
-
-/* StencilFunction */
-const NEVER = 0x0200;
-const LESS = 0x0201;
-const EQUAL = 0x0202;
-const LEQUAL = 0x0203;
-const GREATER = 0x0204;
-const NOTEQUAL = 0x0205;
-const GEQUAL = 0x0206;
-const ALWAYS = 0x0207;
-
-/* StencilOp */
-/* ZERO */
-const KEEP = 0x1e00;
-const REPLACE = 0x1e01;
-const INCR = 0x1e02;
-const DECR = 0x1e03;
-const INVERT = 0x150a;
-const INCR_WRAP = 0x8507;
-const DECR_WRAP = 0x8508;
-
-/* StringName */
-const VENDOR = 0x1f00;
-const RENDERER = 0x1f01;
-const VERSION = 0x1f02;
-
-/* TextureMagFilter */
-const NEAREST = 0x2600;
-const LINEAR = 0x2601;
-
-/* TextureMinFilter */
-/* NEAREST */
-/* LINEAR */
-const NEAREST_MIPMAP_NEAREST = 0x2700;
-const LINEAR_MIPMAP_NEAREST = 0x2701;
-const NEAREST_MIPMAP_LINEAR = 0x2702;
-const LINEAR_MIPMAP_LINEAR = 0x2703;
-
-/* TextureParameterName */
-const TEXTURE_MAG_FILTER = 0x2800;
-const TEXTURE_MIN_FILTER = 0x2801;
-const TEXTURE_WRAP_S = 0x2802;
-const TEXTURE_WRAP_T = 0x2803;
-
-/* TextureTarget */
-const TEXTURE_2D = 0x0de1;
-const TEXTURE = 0x1702;
-
-const TEXTURE_CUBE_MAP = 0x8513;
-const TEXTURE_BINDING_CUBE_MAP = 0x8514;
-const TEXTURE_CUBE_MAP_POSITIVE_X = 0x8515;
-const TEXTURE_CUBE_MAP_NEGATIVE_X = 0x8516;
-const TEXTURE_CUBE_MAP_POSITIVE_Y = 0x8517;
-const TEXTURE_CUBE_MAP_NEGATIVE_Y = 0x8518;
-const TEXTURE_CUBE_MAP_POSITIVE_Z = 0x8519;
-const TEXTURE_CUBE_MAP_NEGATIVE_Z = 0x851a;
-const MAX_CUBE_MAP_TEXTURE_SIZE = 0x851c;
-
-/* TextureUnit */
-const TEXTURE0 = 0x84c0;
-const TEXTURE1 = 0x84c1;
-const TEXTURE2 = 0x84c2;
-const TEXTURE3 = 0x84c3;
-const TEXTURE4 = 0x84c4;
-const TEXTURE5 = 0x84c5;
-const TEXTURE6 = 0x84c6;
-const TEXTURE7 = 0x84c7;
-const TEXTURE8 = 0x84c8;
-const TEXTURE9 = 0x84c9;
-const TEXTURE10 = 0x84ca;
-const TEXTURE11 = 0x84cb;
-const TEXTURE12 = 0x84cc;
-const TEXTURE13 = 0x84cd;
-const TEXTURE14 = 0x84ce;
-const TEXTURE15 = 0x84cf;
-const TEXTURE16 = 0x84d0;
-const TEXTURE17 = 0x84d1;
-const TEXTURE18 = 0x84d2;
-const TEXTURE19 = 0x84d3;
-const TEXTURE20 = 0x84d4;
-const TEXTURE21 = 0x84d5;
-const TEXTURE22 = 0x84d6;
-const TEXTURE23 = 0x84d7;
-const TEXTURE24 = 0x84d8;
-const TEXTURE25 = 0x84d9;
-const TEXTURE26 = 0x84da;
-const TEXTURE27 = 0x84db;
-const TEXTURE28 = 0x84dc;
-const TEXTURE29 = 0x84dd;
-const TEXTURE30 = 0x84de;
-const TEXTURE31 = 0x84df;
-const ACTIVE_TEXTURE = 0x84e0;
-
-/* TextureWrapMode */
-const REPEAT = 0x2901;
-const CLAMP_TO_EDGE = 0x812f;
-const MIRRORED_REPEAT = 0x8370;
-
-/* Uniform Types */
-const FLOAT_VEC2 = 0x8b50;
-const FLOAT_VEC3 = 0x8b51;
-const FLOAT_VEC4 = 0x8b52;
-const INT_VEC2 = 0x8b53;
-const INT_VEC3 = 0x8b54;
-const INT_VEC4 = 0x8b55;
-const BOOL = 0x8b56;
-const BOOL_VEC2 = 0x8b57;
-const BOOL_VEC3 = 0x8b58;
-const BOOL_VEC4 = 0x8b59;
-const FLOAT_MAT2 = 0x8b5a;
-const FLOAT_MAT3 = 0x8b5b;
-const FLOAT_MAT4 = 0x8b5c;
-const SAMPLER_2D = 0x8b5e;
-const SAMPLER_CUBE = 0x8b60;
-
-/* Vertex Arrays */
-const VERTEX_ATTRIB_ARRAY_ENABLED = 0x8622;
-const VERTEX_ATTRIB_ARRAY_SIZE = 0x8623;
-const VERTEX_ATTRIB_ARRAY_STRIDE = 0x8624;
-const VERTEX_ATTRIB_ARRAY_TYPE = 0x8625;
-const VERTEX_ATTRIB_ARRAY_NORMALIZED = 0x886a;
-const VERTEX_ATTRIB_ARRAY_POINTER = 0x8645;
-const VERTEX_ATTRIB_ARRAY_BUFFER_BINDING = 0x889f;
-
-/* Read Format */
-const IMPLEMENTATION_COLOR_READ_TYPE = 0x8b9a;
-const IMPLEMENTATION_COLOR_READ_FORMAT = 0x8b9b;
-
-/* Shader Source */
-const COMPILE_STATUS = 0x8b81;
-
-/* Shader Precision-Specified Types */
-const LOW_FLOAT = 0x8df0;
-const MEDIUM_FLOAT = 0x8df1;
-const HIGH_FLOAT = 0x8df2;
-const LOW_INT = 0x8df3;
-const MEDIUM_INT = 0x8df4;
-const HIGH_INT = 0x8df5;
-
-/* Framebuffer Object. */
-const FRAMEBUFFER = 0x8d40;
-const RENDERBUFFER = 0x8d41;
-
-const RGBA4 = 0x8056;
-const RGB5_A1 = 0x8057;
-const RGB565 = 0x8d62;
-const DEPTH_COMPONENT16 = 0x81a5;
-const STENCIL_INDEX8 = 0x8d48;
-const DEPTH_STENCIL = 0x84f9;
-
-const RENDERBUFFER_WIDTH = 0x8d42;
-const RENDERBUFFER_HEIGHT = 0x8d43;
-const RENDERBUFFER_INTERNAL_FORMAT = 0x8d44;
-const RENDERBUFFER_RED_SIZE = 0x8d50;
-const RENDERBUFFER_GREEN_SIZE = 0x8d51;
-const RENDERBUFFER_BLUE_SIZE = 0x8d52;
-const RENDERBUFFER_ALPHA_SIZE = 0x8d53;
-const RENDERBUFFER_DEPTH_SIZE = 0x8d54;
-const RENDERBUFFER_STENCIL_SIZE = 0x8d55;
-
-const FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE = 0x8cd0;
-const FRAMEBUFFER_ATTACHMENT_OBJECT_NAME = 0x8cd1;
-const FRAMEBUFFER_ATTACHMENT_TEXTURE_LEVEL = 0x8cd2;
-const FRAMEBUFFER_ATTACHMENT_TEXTURE_CUBE_MAP_FACE = 0x8cd3;
-
-const COLOR_ATTACHMENT0 = 0x8ce0;
-const DEPTH_ATTACHMENT = 0x8d00;
-const STENCIL_ATTACHMENT = 0x8d20;
-const DEPTH_STENCIL_ATTACHMENT = 0x821a;
-
-const NONE = 0;
-
-const FRAMEBUFFER_COMPLETE = 0x8cd5;
-const FRAMEBUFFER_INCOMPLETE_ATTACHMENT = 0x8cd6;
-const FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT = 0x8cd7;
-const FRAMEBUFFER_INCOMPLETE_DIMENSIONS = 0x8cd9;
-const FRAMEBUFFER_UNSUPPORTED = 0x8cdd;
-
-const FRAMEBUFFER_BINDING = 0x8ca6;
-const RENDERBUFFER_BINDING = 0x8ca7;
-const MAX_RENDERBUFFER_SIZE = 0x84e8;
-
-const INVALID_FRAMEBUFFER_OPERATION = 0x0506;
-
-/* WebGL-specific enums */
-const UNPACK_FLIP_Y_WEBGL = 0x9240;
-const UNPACK_PREMULTIPLY_ALPHA_WEBGL = 0x9241;
-const CONTEXT_LOST_WEBGL = 0x9242;
-const UNPACK_COLORSPACE_CONVERSION_WEBGL = 0x9243;
-const BROWSER_DEFAULT_WEBGL = 0x9244;
-
-const SRGB_EXT = 0x8c40;
-const SRGB_ALPHA_EXT = 0x8c42;
-const SRGB8_ALPHA8_EXT = 0x8c43;
-const FRAMEBUFFER_ATTACHMENT_COLOR_ENCODING_EXT = 0x8210;
 
 export enum FaceCulling {
     Front = FRONT,
